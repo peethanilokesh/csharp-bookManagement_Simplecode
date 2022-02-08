4efa60241f58acddeb9eafd2f69969394159f35f
lokeshpeethani2 <lokeshpeethani2@gmail.com>
2022-02-08 02:58:52 +0000

    Add project files.

---
+++ Csharp-assessment-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Csharp_assessment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
